0b8183bd6131d47d94c87943358fb156ad76d61e
iamkervyntan <kervyntan@gmail.com>
2022-10-28 01:02:40 +0800

    update

--- leave_app/src/components/Home.jsx
+++ leave_app/src/components/Home.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import AddStaff from './AddStaff';
+import {db} from "../firebase";
+import {
+    collection,
+    addDoc,
+    serverTimestamp,
+    updateDoc,
+    doc,
+    getDocs
+  } from "firebase/firestore"
 
 const Home = () => {
     // need to have a home dashboard
@@ -11,9 +20,50 @@ const Home = () => {
     // will be deducted 
     // need to count the number of days between start date and end date of date picker** (for deduction)
     // if person has 0 days of that leave, cannot allow them to submit the form'
+    const [staffDetails, setStaffDetails] = useState([{
+        name : "",
+        annual_leave : "",
+        compassionate_leave : "",
+        no_pay_leave : "",
+        paternity_leave : "",
+        maternity_leave : ""
+    }]);
+    let arr = [];
+    const colRef = collection(db, "staff");
+    // setStaffDetails(db.collection('staff').get());
+    // console.log(staffDetails);
+    useEffect( () => {
+        getDocs(colRef).then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                // setStaffDetails(...staffDetails, 
+                //     doc.data()
+                // )
+                // arr.push(doc.data());
+                setStaffDetails([...staffDetails, {
+                    name : doc.data().name,
+                    annual_leave : doc.data().annual_leave,
+                    compassionate_leave : doc.data().compassionate_leave,
+                    no_pay_leave : doc.data().no_pay_leave,
+                    paternity_leave : doc.data().paternity_leave,
+                    maternity_leave : doc.data().maternity_leave
+                }])
+            })
+        })
+    }, [])
+
+        // setStaffDetails(arr);
+
+    console.log(staffDetails);
     return (
         <div className="container">
-
+            <div className="staff">
+                <h1 className="name">Name</h1>
+                <h1 className="annual_leave">Annual:</h1>
+                <h1 className="compassionate_leave">Compassionate: </h1>
+                <h1 className="no_pay_leave">No Pay: </h1>
+                <h1 className="paternity_leave">Paternity: </h1>
+                <h1 className="maternity_leave">Maternity: </h1>
+            </div>
         </div>
     )
 }
