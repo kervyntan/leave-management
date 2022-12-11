17037ddc7c015cbc151840d0ef4ca10879b9c5a2
iamkervyntan <kervyntan@gmail.com>
2022-12-11 20:55:17 +0800

    updated SCOPE of gapi

--- leave_app/src/gapi.js
+++ leave_app/src/gapi.js
@@ -2,6 +2,6 @@ let gapi = window.gapi
 let CLIENT_ID = '611831219544-gt7jep7a60aftt86skvtrbclij0al7g0.apps.googleusercontent.com';
 let API_KEY = 'AIzaSyA70FSQw1ygWDNfla5EI9VbOIxhqZozdyk';
 let DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
-let SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+let SCOPES = 'https://www.googleapis.com/auth/calendar.events';
 
 export {gapi, CLIENT_ID, API_KEY, DISCOVERY_DOC, SCOPES}
